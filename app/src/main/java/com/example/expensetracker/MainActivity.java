36cee5456b237d56c210422d4df24a520a96befa
CurfewOvernight <curfewovernight@gmail.com>
2021-10-01 23:04:16 +0530

    1st commit

---
+++ app/src/main/java/com/example/expensetracker/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.expensetracker;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        getSupportFragmentManager().beginTransaction().replace(R.id.main_activity_container, new HomeFragment()).commit();
+
+        bottomNavigationView.setSelectedItemId(R.id.nav_home);
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment = null;
+
+                switch (item.getItemId()) {
+                    case R.id.nav_home:
+                        fragment = new HomeFragment();
+                        break;
+                    case R.id.nav_expenses:
+                        fragment = new ExpensesFragment();
+                        break;
+                    case R.id.nav_wallet:
+                        fragment = new MoneySourceFragment();
+                        break;
+                    case R.id.nav_settings:
+                        fragment = new SettingsFragment();
+                        break;
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.main_activity_container, fragment).commit();
+
+                return true;
+            }
+        });
+    }
+}
